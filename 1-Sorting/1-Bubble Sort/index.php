a1d063eaa2c8d5090c05239384c70fd185f6d3c1
enes toy <79420918+enestoy@users.noreply.github.com>
2025-02-27 17:12:14 +0300

    sorting

--- 1-Sorting/1-Bubble Sort/index.php
+++ 1-Sorting/1-Bubble Sort/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Bubble Sort (Kabarcık Sıralama) fonksiyonu
-function kabarcikSirala($dizi) {
+function bubbleSort($dizi) {
     $elemanSayisi = count($dizi);
     
     for ($i = 0; $i < $elemanSayisi - 1; $i++) {
@@ -23,7 +23,7 @@ function kabarcikSirala($dizi) {
 $sayilar = [5, 3, 8, 4, 2];
 
 // Sıralama işlemi
-$siralanmisSayilar = kabarcikSirala($sayilar);
+$siralanmisSayilar = bubbleSort($sayilar);
 
 // Sonucu ekrana yazdırma
 echo "<pre style='color:white; background-color:rgb(9, 117, 18); border: 1px solid #ccc; padding: 10px;'>";
