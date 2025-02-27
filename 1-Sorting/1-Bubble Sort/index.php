6bd45a4b1cf172ade4c203b8a61ef79beeb249b9
enes toy <79420918+enestoy@users.noreply.github.com>
2025-02-27 17:09:52 +0300

    sorting

--- 1-Sorting/1-Bubble Sort/index.php
+++ 1-Sorting/1-Bubble Sort/index.php
@@ -1,31 +1,34 @@
 <?php
 
-// Bubble Sort fonksiyonu
-function bubbleSort($arr) {
-    $n = count($arr);
-    for ($i = 0; $i < $n - 1; $i++) {
-        // Her geçişte, büyük öğeler sona kayar
-        for ($j = 0; $j < $n - 1 - $i; $j++) {
-            if ($arr[$j] > $arr[$j + 1]) {
-                // Yer değiştirme
-                $temp = $arr[$j];
-                $arr[$j] = $arr[$j + 1];
-                $arr[$j + 1] = $temp;
+// Bubble Sort (Kabarcık Sıralama) fonksiyonu
+function kabarcikSirala($dizi) {
+    $elemanSayisi = count($dizi);
+    
+    for ($i = 0; $i < $elemanSayisi - 1; $i++) {
+        // Her geçişte en büyük öğeler sona doğru kaydırılır
+        for ($j = 0; $j < $elemanSayisi - 1 - $i; $j++) {
+            if ($dizi[$j] > $dizi[$j + 1]) {
+                // Yer değişimi yapılır
+                $gecici = $dizi[$j];
+                $dizi[$j] = $dizi[$j + 1];
+                $dizi[$j + 1] = $gecici;
             }
         }
     }
-    return $arr;
+    
+    return $dizi;
 }
 
-// Test listesi
-$numbers = [5, 3, 8, 4, 2];
+// Test için örnek sayı listesi
+$sayilar = [5, 3, 8, 4, 2];
 
-// Sıralama
-$sortedNumbers = bubbleSort($numbers);
+// Sıralama işlemi
+$siralanmisSayilar = kabarcikSirala($sayilar);
 
-// Sonucu yazdırma
-echo "<pre style='color:white;background-color:rgb(9, 117, 18); border: 1px solid #ccc; padding: 10px;'>";
-echo "Sıralı Liste: ";
-print_r($sortedNumbers);
+// Sonucu ekrana yazdırma
+echo "<pre style='color:white; background-color:rgb(9, 117, 18); border: 1px solid #ccc; padding: 10px;'>";
+echo "Sıralanmış Liste: ";
+print_r($siralanmisSayilar);
 echo "</pre>";
+
 ?>
